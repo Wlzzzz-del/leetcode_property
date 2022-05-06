3f70e5ed196df067a62ff8cf19993328820f7993
Wlzzzz-del <1174547806@qq.com>
2022-05-06 08:36:24 +0800

    add 933\713

--- 713_numSubarrayProductLessThanK.cpp
+++ 713_numSubarrayProductLessThanK.cpp
@@ -1,5 +1,5 @@
 // https://leetcode-cn.com/problems/subarray-product-less-than-k/
-// 给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+// 给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目
 #include <iostream>
 #include <vector>
 
@@ -23,9 +23,31 @@ public:
         }
 };
 
+class Solution1{
+    public:
+    int numSubarrayProductLessThanK(std::vector<int> vec, int k){
+        int len = vec.size();
+        int all = 1;
+        int count = 0;
+        int j=0;
+        for(int i=0;i<len; ++i){
+            all *= vec[i];
+            // 移动j指针
+            while(all>=k && j<i){
+                std::cout<<vec[j]<<" ";
+                std::cout<<all<<std::endl;
+                all /= vec[j];
+                ++j;
+            }
+            count += i-j+1;
+        }
+        return count;
+    }
+};
 int main(){
-    Solution s;
-    std::vector<int> vec({10,9,10,4,3,8,3,3,6,2,10,10,9,3});
-    s.numSubarrayProductLessThanK(vec,19);
+    Solution1 s;
+    // std::vector<int> vec({10,9,10,4,3,8,3,3,6,2,10,10,9,3});
+    std::vector<int> vec({10,5,2,6});
+    s.numSubarrayProductLessThanK(vec,100);
     system("pause");
 }
